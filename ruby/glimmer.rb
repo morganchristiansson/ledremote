8840ca33796dd193c22b68e277e49d56541734f3
Morgan Christiansson <code@mog.se>
2014-05-12 18:50:17 +0100

    Update glimmer effect to have variable multiplier

--- ruby/glimmer.rb
+++ ruby/glimmer.rb
@@ -6,9 +6,12 @@ C=[80,255,0]
 
 clear!
 
+@speed = 0
+@multiplier = 0.9
 (0...10000).cycle.each do |offset|
   (0...NUM_PIXELS).each do |i|
-    setpixel(i, C.map { |c| (c * Math.sin(i/1.1+offset/20.0)).to_i.abs })
+    @multiplier -= 0.000_000_1
+    setpixel(i, C.map { |c| (c * Math.sin(i*@multiplier-offset*@speed)).to_i.abs })
   end
   show $p
 end
