122b29f0d968a7c72c97aebd3228c6f8868a4695
Morgan Christiansson <code@mog.se>
2014-09-29 16:47:18 +0100

    Add different types of commands to server

--- ruby/server.rb
+++ ruby/server.rb
@@ -5,18 +5,37 @@ require 'socket'
 wait_for_boot
 
 server = TCPServer.new 2000
+$proc = lambda { [0,0,0]*NUM_PIXELS }
+Thread.new { main_loop }
+
 loop do
   begin
     STDERR.puts 'waiting for connection'
     client = server.accept
-    while line = client.gets
+    Thread.new do
       begin
-        rgb = *line.split(' ').map(&:to_i)
-        $stderr.puts "rgb = #{rgb.inspect}"
-        #$led.set *rgb
-        show rgb*NUM_PIXELS
-        client.puts "ok"
+        while line = client.gets
+          cmd, line = line.split(' ', 2)
+          $stderr.puts "Received command: #{cmd}: #{line}"
+
+          case cmd
+          when 'RGB'
+            rgb = *line.split(' ').map(&:to_i)
+            $stderr.puts "rgb = #{rgb.inspect}"
+            #$led.set *rgb
+            $proc = lambda { rgb*NUM_PIXELS }
+            client.puts "ok"
+          when 'EFFECT'
+            $stderr.puts "EFFECT"
+            $proc = RainbowWheel.new
+            client.puts "ok"
+          else
+            $stderr.puts "Unknown command: #{cmd}: #{line}"
+          end
+        end
       rescue
+        $stderr.puts $!
+        $stderr.puts $!.backtrace.join "\n"
         client.puts $!
         client.puts $!.backtrace.join "\n"
       end
