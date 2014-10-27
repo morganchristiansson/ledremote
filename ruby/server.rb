9d9ece9655639727d18a80230a2e238e1abebcff
Morgan Christiansson <code@mog.se>
2014-10-27 09:11:27 +0000

    Add a pry console to tcp server

--- ruby/server.rb
+++ ruby/server.rb
@@ -2,46 +2,51 @@
 require './lib/setup'
 require 'socket'
 
+require 'pry'
+
 wait_for_boot
 
 server = TCPServer.new 2000
 $proc = lambda { [0,0,0]*NUM_PIXELS }
-Thread.new { main_loop }
+main_thread = Thread.new { main_loop }
 
-loop do
-  begin
-    STDERR.puts 'waiting for connection'
-    client = server.accept
-    Thread.new do
-      begin
-        while line = client.gets
-          cmd, line = line.split(' ', 2)
-          $stderr.puts "Received command: #{cmd}: #{line}"
+server_thread = Thread.new do
+  loop do
+    begin
+      STDERR.puts 'waiting for connection'
+      client = server.accept
+      Thread.new do
+        begin
+          while line = client.gets
+            cmd, line = line.split(' ', 2)
+            $stderr.puts "Received command: #{cmd}: #{line}"
 
-          case cmd
-          when 'RGB'
-            rgb = *line.split(' ').map(&:to_i)
-            $stderr.puts "rgb = #{rgb.inspect}"
-            #$led.set *rgb
-            $proc = lambda { rgb*NUM_PIXELS }
-            client.puts "ok"
-          when 'EFFECT'
-            $stderr.puts "EFFECT"
-            $proc = RainbowWheel.new
-            client.puts "ok"
-          else
-            $stderr.puts "Unknown command: #{cmd}: #{line}"
+            case cmd
+            when 'RGB'
+              rgb = *line.split(' ').map(&:to_i)
+              $stderr.puts "rgb = #{rgb.inspect}"
+              #$led.set *rgb
+              $proc = lambda { rgb*NUM_PIXELS }
+              client.puts "ok"
+            when 'EFFECT'
+              $stderr.puts "EFFECT"
+              $proc = RainbowWheel.new
+              client.puts "ok"
+            else
+              $stderr.puts "Unknown command: #{cmd}: #{line}"
+            end
           end
+        rescue
+          $stderr.puts $!
+          $stderr.puts $!.backtrace.join "\n"
+          client.puts $!
+          client.puts $!.backtrace.join "\n"
         end
-      rescue
-        $stderr.puts $!
-        $stderr.puts $!.backtrace.join "\n"
-        client.puts $!
-        client.puts $!.backtrace.join "\n"
       end
+    rescue
+      $stderr.puts $!
     end
-  rescue
-    $stderr.puts $!
   end
 end
 
+binding.pry
